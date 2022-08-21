80390c390bbb790a92a40091b60e8400d38fd2e7
Felix Kaaman <tmtu@tmtu.ee>
2022-08-21 21:26:34 +0200

    Example of HiveID integration

--- backend/scuffed/migrations/0001_initial.sql
+++ backend/scuffed/migrations/0001_initial.sql
@@ -1,18 +1,4 @@
 CREATE TABLE users (
     username TEXT PRIMARY KEY NOT NULL COLLATE NOCASE,
-    created_at INTEGER NOT NULL,
-    password_hash TEXT NOT NULL,
     stream_key TEXT NOT NULL
-) STRICT;
-
-CREATE TABLE user_sessions (
-    id INTEGER PRIMARY KEY NOT NULL,
-    username TEXT NOT NULL,
-    token TEXT NOT NULL UNIQUE,
-    created_at INTEGER NOT NULL,
-
-    CONSTRAINT fk_username_assoc
-        FOREIGN KEY (username)
-        REFERENCES users (username)
-        ON DELETE CASCADE
 ) STRICT;
--- backend/scuffed/src/main.rs
+++ backend/scuffed/src/main.rs
@@ -1,11 +1,25 @@
 #![allow(dead_code)]
-use std::env;
 use std::net::SocketAddr;
 use std::path::PathBuf;
+use std::sync::Arc;
+use std::{borrow::Cow, env};
 
+use anyhow::Context;
+use askama::Template;
+use axum::body::{self, boxed, BoxBody, Empty, Full};
+use axum::response::Response;
+use axum::routing::get;
 use axum::{http::StatusCode, response::IntoResponse, routing::get_service, Extension, Router};
+use futures::FutureExt;
+use idlib::{
+    AuthCallback, AuthorizationRejection, Authorizations, Authorize, IdpClient, PermissionResponse,
+    SecretKey, Variables,
+};
+use jwt::token::signed::SignWithKey;
 use log::*;
+use rusqlite::{params, OptionalExtension};
 use rusqlite_migration::{Migrations, M};
+use serde::Deserialize;
 use tower_http::services::ServeDir;
 
 mod db;
@@ -18,28 +32,204 @@ pub type Connection = tokio_rusqlite::Connection;
 
 const MIGRATIONS: [M; 1] = [M::up(include_str!("../migrations/0001_initial.sql"))];
 
-pub fn api_route(
+pub fn into_response<T: Template>(t: &T, ext: &str) -> Response<BoxBody> {
+    match t.render() {
+        Ok(body) => Response::builder()
+            .status(StatusCode::OK)
+            .header("content-type", "text/html")
+            .body(body::boxed(Full::from(body)))
+            .unwrap(),
+        Err(_) => Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(body::boxed(Empty::new()))
+            .unwrap(),
+    }
+}
+
+async fn create_account_if_missing(db: Connection, name: String) -> anyhow::Result<()> {
+    db.call(move |conn| {
+        if let None = conn
+            .query_row(
+                "SELECT * FROM users WHERE username = ?1",
+                params![&name],
+                |r| Ok(()),
+            )
+            .optional()
+            .context("")?
+        {
+            let stream_key = "test123";
+
+            conn.execute(
+                "INSERT INTO users (username, stream_key) VALUES (?1, ?2)",
+                params![&name, &stream_key],
+            )
+            .context("")?;
+
+            info!("Created account for user {name}");
+        } else {
+            debug!("Account already existed for {name}");
+        }
+
+        Ok::<(), anyhow::Error>(())
+    })
+    .await?;
+
+    Ok(())
+}
+
+pub async fn api_route(
     db: tokio_rusqlite::Connection,
     streams: stream::LiveStreams,
     serve_dir: PathBuf,
     old_serve_dir: Option<PathBuf>,
 ) -> Router {
-    let mut router = Router::new()
-        .nest("/api/streams/", stream::api_route())
-        // .route("/api/streams/:stream", get(stream::get_streams))
-        // .route("/api/streams/:stream/snapshot", get())
-        .fallback(get_service(ServeDir::new(serve_dir)).handle_error(handle_error))
-        .layer(Extension(db))
-        .layer(Extension(streams));
+    let secret_key = SecretKey::from_env();
+    let variables = Variables::from_env();
+    let authorizations = Authorizations::in_memory().await;
 
-    if let Some(old_dir) = old_serve_dir {
-        router = router.nest(
-            "/old/",
-            get_service(ServeDir::new(old_dir)).handle_error(handle_error),
-        );
+    let client = IdpClient::default();
+
+    if let Err(e) = fill_auth(&authorizations, &client, &variables, &secret_key).await {
+        log::error!("Failed to fetch authorization info");
     }
 
+    let a = db.clone();
+    let mut router = Router::new()
+        .route("/api/health", get(health))
+        .route("/account", get(account))
+        .nest("/api/streams/", stream::api_route())
+        .nest(
+            "/api/auth",
+            idlib::api_route(
+                client,
+                Some(AuthCallback(Arc::new(Box::new(move |name| {
+                    let db = a.clone();
+
+                    async move {
+                        create_account_if_missing(db, name).await?;
+
+                        Ok(())
+                    }
+                    .boxed()
+                })))),
+            ),
+        )
+        .fallback(get_service(ServeDir::new(serve_dir)).handle_error(handle_error));
+
+    if let Some(old_dir) = old_serve_dir {
+        router = router
+            .nest(
+                "/old/",
+                Router::new()
+                    .route("/", get(home))
+                    .route("/account", get(account)),
+            )
+            .fallback(get_service(ServeDir::new(old_dir)).handle_error(handle_error));
+    }
+
+    router = router
+        .layer(Extension(db))
+        .layer(Extension(streams))
+        .layer(Extension(secret_key))
+        .layer(Extension(authorizations))
+        .layer(Extension(Arc::new(variables)));
+
     router
+}
+
+async fn health() -> Result<Response<BoxBody>, Error> {
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .body(boxed(Empty::new()))
+        .unwrap();
+
+    Ok(response)
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct HomePageTemplate {
+    username: Option<String>,
+}
+
+async fn home(
+    auth: Result<Authorize, AuthorizationRejection>,
+    Extension(db): Extension<Connection>,
+) -> Result<Response<BoxBody>, Error> {
+    let template = HomePageTemplate {
+        username: auth.ok().map(|a| a.0),
+    };
+
+    Ok(into_response(&template, "html"))
+}
+
+#[derive(Template)]
+#[template(path = "account.html")]
+struct AccountPageTemplate<'a> {
+    username: &'a str,
+    stream_key: &'a str,
+}
+
+async fn account(
+    Authorize(name): Authorize,
+    Extension(db): Extension<Connection>,
+) -> Result<Response<BoxBody>, Error> {
+    let stream_key = get_stream_key(db, name.clone()).await?;
+
+    let template = AccountPageTemplate {
+        username: &name,
+        stream_key: &stream_key,
+    };
+
+    Ok(into_response(&template, "html"))
+}
+
+async fn get_stream_key(db: Connection, name: String) -> anyhow::Result<String> {
+    let key = db
+        .call(move |conn| {
+            conn.query_row(
+                "SELECT \
+                stream_key \
+            FROM users \
+            WHERE username = ?1",
+                params![name],
+                |row| row.get::<_, String>(0),
+            )
+        })
+        .await
+        .context("Failed to get inviter")?;
+
+    Ok(key)
+}
+
+async fn fill_auth(
+    auth: &Authorizations,
+    IdpClient(client): &IdpClient,
+    variables: &Variables,
+    key: &SecretKey,
+) -> anyhow::Result<()> {
+    let url = variables
+        .idp_fetch_permission_address
+        .as_ref()
+        .expect("IDP_FETCH_PERMISSION_ADDR not set");
+    let token = "yup"
+        .sign_with_key(&*key.0)
+        .context("Failed to sign token")?;
+
+    let body = client
+        .get(url)
+        .header("Authorization", format!("Bearer {token}"))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let permissions: PermissionResponse = serde_json::de::from_str(&body)?;
+    auth.replace_policy(permissions.policy, permissions.group_policy)
+        .await
+        .context("Failed to replace policy")?;
+
+    Ok(())
 }
 
 async fn run() {
@@ -75,7 +265,7 @@ async fn run() {
         }
     });
 
-    let router = api_route(conn, streams, serve_dir, old_serve_dir);
+    let router = api_route(conn, streams, serve_dir, old_serve_dir).await;
 
     axum::Server::try_bind(&bind_addr)
         .expect("Failed to bind server")
